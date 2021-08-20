07be4bd55a0202ac6f84846fa83960d2edc20ff1
SuhailSherief <suhailscreed@gmail.com>
2021-08-20 15:43:55 +0530

    init

---
+++ main.js
@@ -0,0 +1,5 @@
+const discord = document.querySelector('.discord');
+const gmail = document.querySelector('.gmail');
+
+discord.addEventListener('click', () => alert('LuciFer#1001'));
+gmail.addEventListener('click', () => alert('suhailscreed@gmail.com'));
