1ac3d2aed133fdf916f0a896fc1a09cbe67933d6
シェリエフ <75260836+SuhailSherief@users.noreply.github.com>
2023-01-06 00:13:05 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const discord = document.querySelector('.discord');
 const gmail = document.querySelector('.gmail');
 
-discord.addEventListener('click', () => alert('LuciFer#1001'));
+discord.addEventListener('click', () => alert('シェリエフ#1423'));
 gmail.addEventListener('click', () => alert('suhailscreed@gmail.com'));
